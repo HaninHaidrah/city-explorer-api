8e2bcbcfc3f8be3a41c34fe3ed0717199823a8d1
HaninHaidrah <haneenhadey@gmail.com>
2021-09-08 00:01:12 +0300

    try to do the movies

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 
 const express = require("express");
 const app = express();
-const weatherData = require("./weather.json");
+// const weatherData = require("./weather.json");
 require('dotenv').config();
 const PORT = process.env.PORT;
+const APIKEY= process.env.WEATHER_API_KEY;
 const cors = require('cors');
+const { request, response } = require("express");
 app.use(cors()) 
 
 class Forecast{
@@ -15,12 +17,22 @@ class Forecast{
   }
 }
 
-app.get("/weather", (request, respond) => {
+app.get("/weather", async (request, respond) => {
  try{
    const city_name = request.query.city_name;
-   const UpdatedData = weatherData.find((el) => {
+   const lat=request.query.lat
+   const lon=request.query.lon
+
+   const weatherURL=`https://api.weatherbit.io/v2.0/forecast/daily?city=${city_name}&key=${APIKEY}&lat=${lat}&lon${lon}`
+
+   const gettingTheData= await axios.get(weatherURL)
+    respond.json(gettingTheData);
+
+   const UpdatedData = gettingTheData.find((el) => {
     return(el.city_name.toLowerCase() === city_name) 
-    });
+    })
+   respond.json(UpdatedData);
+    ;
     if(UpdatedData){
       let wantedData=UpdatedData.data.map((el)=>{
       return new Forecast(el.datetime,el.weather.description)
@@ -40,6 +52,25 @@ app.get("/weather", (request, respond) => {
 
 });
 
+
+app.get('/movies',(request,response)=>{
+const city_name=request.query.city_name
+const moviesUrl=`
+https://api.themoviedb.org/3/movie/top_rated?api_key=${APIKEY}&language=en-US&page=1`
+
+
+const getUrl= await axios.get(moviesUrl);
+
+const newData=getUrl.map((el)=>{
+  return el.title;
+})
+if(newData.includes(city_name)){
+  response.send(getUrl)
+}
+else{
+  response.send('no data')
+}
+})
 
 app.listen(PORT, () => {
   console.log({PORT});
